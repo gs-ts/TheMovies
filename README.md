44eb275e41d245b3febba4da66ab0fb00e4be015
gs-ts <john87ts@gmail.com>
2025-05-23 12:24:03 +0300

    added necessary dependencies and updated readme

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-### Technical details
+## Technical details
 
+- Kotlin
 - mindSdk = 25 (Android 7)
 - targetSdk = 36 (Android 16)
+
+### Libraries
+
+- UI: Compose
+- DI: Hilt
+- Network: Ktor
+- Image Loading: Coil
+- Serialization: KotlinXSerialization
+- Paging: Paging3
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -48,22 +51,42 @@ android {
 
 dependencies {
 
+    implementation(libs.kotlin.coroutines)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.paging.runtime)
+    implementation(libs.paging.compose)
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.kotlinx.serialization)
+    implementation(libs.kotlinx.collections)
+    implementation(libs.coil.compose)
+    implementation(libs.accompanist.placeholder)
+
+    ksp(libs.hilt.compiler)
 
     testImplementation(libs.junit)
+    testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.kotlin.coroutines.test)
+    testImplementation(libs.ktor.client.mock)
+    testImplementation(libs.paging.common)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
