fa57fc31fb43605a914e22ae2266f2a156356103
gs-ts <john87ts@gmail.com>
2025-05-24 12:35:52 +0300

    updated getMovies to return MovieListResponse in TmdbApi

--- app/src/main/java/com/example/themovies/data/network/TmdbApi.kt
+++ app/src/main/java/com/example/themovies/data/network/TmdbApi.kt
@@ -1,7 +1,7 @@
 package com.example.themovies.data.network
 
+import com.example.themovies.data.dto.MovieListResponse
 import com.example.themovies.domain.model.Genre
-import com.example.themovies.domain.model.Movie
 import com.example.themovies.domain.model.MovieDetails
 
 interface TmdbApi {
@@ -9,7 +9,7 @@ interface TmdbApi {
     suspend fun getMovies(
         page: Int,
         genreId: Int? = null // if null then get all genres
-    ): Result<List<Movie>>
+    ): MovieListResponse
 
     suspend fun getGenres(): Result<List<Genre>>
 
