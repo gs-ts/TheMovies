f153741d9a909c337d55d1b9e9ac644e20c75cac
gs-ts <john87ts@gmail.com>
2025-05-24 12:38:59 +0300

    added first iteration of FiltersScreen and FiltersViewModel

--- app/src/main/java/com/example/themovies/ui/filters/FiltersScreen.kt
+++ app/src/main/java/com/example/themovies/ui/filters/FiltersScreen.kt
@@ -1,14 +1,93 @@
 package com.example.themovies.ui.filters
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.themovies.domain.model.Genre
 
 @Composable
 fun FiltersScreen(viewModel: FiltersViewModel) {
-    FiltersContent()
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    FiltersContent(
+        state = state,
+        onFilterSelect = viewModel::onFilterSelect
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun FiltersContent(
+    state: FiltersViewModel.State,
+    onFilterSelect: (Int) -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = Color.Black,
+                    titleContentColor = Color.White,
+                ),
+                title = {
+                    Text("Filters")
+                }
+            )
+        }
+    ) { innerPadding ->
+        if (state.isLoading) {
+            Box(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        if (state.hasError) {
+            Box(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "An error occurred")
+            }
+        }
+
+        if (state.genres.isNotEmpty()) {
+            Column(modifier = Modifier.padding(innerPadding).fillMaxSize()) {
+                state.genres.forEach { genre ->
+                    FilterItem(
+                        genre = genre,
+                        onFilterSelect = {
+                            onFilterSelect(it)
+                        }
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Composable
-private fun FiltersContent() {
-    Text(text = "Filters")
+private fun FilterItem(
+    genre: Genre,
+    onFilterSelect: (Int) -> Unit
+) {
+    Text(text = genre.name)
 }
--- app/src/main/java/com/example/themovies/ui/filters/FiltersViewModel.kt
+++ app/src/main/java/com/example/themovies/ui/filters/FiltersViewModel.kt
@@ -1,8 +1,51 @@
 package com.example.themovies.ui.filters
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.themovies.domain.model.Genre
+import com.example.themovies.domain.usecase.GetMovieGenresUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class FiltersViewModel @Inject constructor() : ViewModel()
+class FiltersViewModel @Inject constructor(
+    private val getMovieGenresUseCase: GetMovieGenresUseCase
+) : ViewModel() {
+
+    private val _state = MutableStateFlow<State>(State())
+    val state: StateFlow<State> = _state.asStateFlow()
+
+    init {
+        _state.value = _state.value.copy(isLoading = true)
+
+        viewModelScope.launch {
+            getMovieGenresUseCase()
+                .onSuccess { genres ->
+                    _state.value = _state.value.copy(
+                        isLoading = false,
+                        genres = genres
+                    )
+                }
+                .onFailure { exception ->
+                    _state.value = _state.value.copy(
+                        isLoading = false,
+                        hasError = true
+                    )
+                }
+        }
+    }
+
+    fun onFilterSelect(genreId: Int) {
+        // TODO or navigate back with id
+    }
+
+    data class State(
+        val isLoading: Boolean = true,
+        val genres: List<Genre> = emptyList(),
+        val hasError: Boolean = false
+    )
+}
