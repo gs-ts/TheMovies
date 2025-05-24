b3e2545426222d3bea64d7530f6b5611ee9b2608
gs-ts <john87ts@gmail.com>
2025-05-24 12:38:38 +0300

    added first iteration of MoviesViewModel

--- app/src/main/java/com/example/themovies/ui/movies/MoviesViewModel.kt
+++ app/src/main/java/com/example/themovies/ui/movies/MoviesViewModel.kt
@@ -1,8 +1,29 @@
 package com.example.themovies.ui.movies
 
 import androidx.lifecycle.ViewModel
+import androidx.paging.map
+import com.example.themovies.domain.usecase.GetMoviesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
-class MoviesViewModel @Inject constructor() : ViewModel()
+class MoviesViewModel @Inject constructor(
+    getMoviesUseCase: GetMoviesUseCase
+) : ViewModel() {
+
+    val movies = getMoviesUseCase(page = 1)
+        .map { pagingData ->
+            pagingData.map { movie ->
+                MovieItem(
+                    id = movie.id,
+                    title = movie.title
+                )
+            }
+        }
+
+    data class MovieItem(
+        val id: Int,
+        val title: String
+    )
+}
